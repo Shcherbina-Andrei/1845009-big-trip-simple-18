e130d0a5fcab02a292ed18f8678d5f7a45fd4a28
andrei110295@yandex.ru <andrei110295@yandex.ru>
2022-08-26 19:50:31 +0300

    Шаблонизируй то

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,14 +1,14 @@
 import AbstractView from '../framework/view/abstract-view';
 
-const createNewFilterTemplate = function() {
+const createNewFilterTemplate = function(points) {
   return `<form class="trip-filters" action="#" method="get">
     <div class="trip-filters__filter">
-      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
+      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" ${points.length === 0 ? 'disabled' : ''}>
       <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
     </div>
 
     <div class="trip-filters__filter">
-      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future" ${points.length === 0 ? 'disabled' : ''}>
       <label class="trip-filters__filter-label" for="filter-future">Future</label>
     </div>
 
@@ -17,7 +17,14 @@ const createNewFilterTemplate = function() {
 };
 
 export default class FilterView extends AbstractView {
+  #points = null;
+
+  constructor(points) {
+    super();
+    this.#points = points;
+  }
+
   get template() {
-    return createNewFilterTemplate();
+    return createNewFilterTemplate(this.#points);
   }
 }
